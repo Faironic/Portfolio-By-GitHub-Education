febb82b275d473772c3cc3f0315b6c95401253ad
Faze <faiyazahmedoyon2006@gmail.com>
2023-07-24 19:25:12 +0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
+  name: "Faiyaz Ahmed",
+  title: "An O level student",
+  email: "faiyaz.io@yahoo.come",
+  gitHub: "iFayaz",
+  instagram: "faiyaz.io",
   linkedIn: "satyanadella",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "2006_faiyaz",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
